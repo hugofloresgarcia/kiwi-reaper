477d3c9300e86277fc01d30f7c1499c36faa6521
hugofloresgarcia <hf01049@georgiasouthern.edu>
2022-02-08 12:55:44 -0600

    more controller stuff

--- src/controller.h
+++ src/controller.h
@@ -1,25 +1,38 @@
 #include "reaper_plugin_functions.h"
 #include "osc.h"
 
-
 class OSCController : public IReaperControlSurface {
 
 public: 
-    const char* GetTypeString() override { return ""; }
-    const char* GetDescString() override { return ""; }
-    const char* GetConfigString() override { return ""; }
+  const char* GetTypeString() override { return "HapticScrubber"; }
+  const char* GetDescString() override { return "A tool for scrubbing audio using haptic feedback."; }
+  const char* GetConfigString() override { return ""; }
 
-    OSCController(OSCManager manager):
-        m_manager(manager) {
+  OSCController(OSCManager manager):
+    m_manager(manager) {
+  }
 
-    }
+  // TODO: should switch focus to last selected track
+  void OnTrackSelection(MediaTrack *trackid) override;
+  
+  // use this to register all callbacks with the osc manager
+  void add_callbacks() {
+    using Msg = oscpkt::Message;
 
-    void Run() override {
-        // this runs about 30x per second. do all OSC polling here
+    // add a callback to listen to
+    m_manager.add_callback(
+      "/example", [](Msg& msg){
         
-    }
+    });
+  };
+
+  // this runs about 30x per second. do all OSC polling here
+  void Run() override {
+    // handle any packets
+    m_manager.handle_receive(false);
+  }
 
 private:
-    OSCManager m_manager;
+  OSCManager m_manager;
 
 };
